df10b61209d498f5339ad58f51a4df2e2b9be768
Abhimanyu Sharma <abhimanyu254a@gmail.com>
2025-11-24 16:04:03 +0530

    Done some changes into the premake and also the moved from the dynamic linking to static linking

--- Shunya-Core/src/Core/Core.h
+++ Shunya-Core/src/Core/Core.h
@@ -1,24 +1,28 @@
 #pragma once
 
 #ifdef SHUNYA_CORE_EXPORTS
-	#ifdef	SHUNYA_BUILD_DLL	
-		#define SHUNYA_API __declspec(dllexport)
+#if SHUNYA_DYNLIB
+#ifdef	SHUNYA_BUILD_DLL	
+#define SHUNYA_API __declspec(dllexport)
 
-	#else
-		#define SHUNYA_API __declspec(dllimport)
-	
-	#endif
 #else
-	#error SHUNYA_CORE_EXPORTS not defined
+#define SHUNYA_API __declspec(dllimport)
+
+#endif
+#else
+#define SHUNYA_API
+#endif
+#else
+#error SHUNYA_CORE_EXPORTS not defined
 
 #endif
 
 #ifdef SHUNYA_ENABLE_ASSERTS
-	#define	SHUNYA_ASSERT(x, ...) {if(!(x)) {SHUNYA_ERROR("Assertion Failed : {0}", __VA__ARGS__); __debugbreak(); }}
-	#define SHUNYA_CORE_ASSERT(x , ...) {if(!x){SHUNYA_CORE_ERROR("Assertion Failed : {0}",__VA__ARGS__); __debugbreak(); } }
+#define	SHUNYA_ASSERT(x, ...) {if(!(x)) {SHUNYA_ERROR("Assertion Failed : {0}", __VA__ARGS__); __debugbreak(); }}
+#define SHUNYA_CORE_ASSERT(x , ...) {if(!x){SHUNYA_CORE_ERROR("Assertion Failed : {0}",__VA__ARGS__); __debugbreak(); } }
 #else
-	#define SHUNYA_ASSERT(x, ...)
-	#define SHUNYA_CORE_ASSERT(x, ...)
+#define SHUNYA_ASSERT(x, ...)
+#define SHUNYA_CORE_ASSERT(x, ...)
 #endif
 
 
