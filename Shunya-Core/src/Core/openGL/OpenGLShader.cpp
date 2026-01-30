0678b7f88d0da56c6225ab5ee0de3c811fa1791d
Abhimanyu Sharma <abhimanyu254a@gmail.com>
2026-01-31 02:53:20 +0530

    Done with the Shader Assets file.

--- Shunya-Core/src/Core/openGL/OpenGLShader.cpp
+++ Shunya-Core/src/Core/openGL/OpenGLShader.cpp
@@ -3,89 +3,145 @@
 #include "OpenGLShader.h"
 #include <glad/glad.h>
 
+#include<fstream>
+
 #include <glm/gtc/type_ptr.hpp>
 
 namespace Shunya
 {
+	static GLenum ShaderTypeFromString(const std::string& type)
+	{
+		if (type == "vertex")
+			return GL_VERTEX_SHADER;
+		if (type == "fragment" || type == "pixel")
+			return GL_FRAGMENT_SHADER;
+
+		SHUNYA_CORE_ASSERT(false, "Unkown shader type!");
+		
+		return 0;
+	}
+
+	OpenGLShader::OpenGLShader(const std::string& filepath)
+	{
+		std::string Source = ReadFile(filepath);
+		auto shaderSources = PreProcess(Source);
+		Compile(shaderSources);
+
+	}
 
 	OpenGLShader::OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
-		// Create an empty vertex shader handle
-		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		std::unordered_map<GLenum, std::string> sources;
+		sources[GL_VERTEX_SHADER] = vertexSrc;
+		sources[GL_FRAGMENT_SHADER] = fragmentSrc;
 
-		// Send the vertex shader source code to GL
-		// Note that std::string's .c_str is NULL character terminated.
-		const GLchar* source = vertexSrc.c_str();
-		glShaderSource(vertexShader, 1, &source, 0);
+		Compile(sources);
+	}
 
-		// Compile the vertex shader
-		glCompileShader(vertexShader);
 
-		GLint isCompiled = 0;
-		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
-		if (isCompiled == GL_FALSE)
+
+	OpenGLShader::~OpenGLShader()
+	{
+
+		glDeleteProgram(m_RendererID);
+
+	}
+	std::string OpenGLShader::ReadFile(const std::string& filepath)
+	{
+		std::string result;
+		std::ifstream in(filepath, std::ios::in, std::ios::binary);
+
+		if (in)
 		{
-			GLint maxLength = 0;
-			glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
-
-			// The maxLength includes the NULL character
-			std::vector<GLchar> infoLog(maxLength);
-			glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
-
-			// We don't need the shader anymore.
-			glDeleteShader(vertexShader);
-
-			// Use the infoLog as you see fit.
-			SHUNYA_CORE_ERROR("{0}", infoLog.data());
-
-			// In this simple program, we'll just leave
-			return;
+			in.seekg(0, std::ios::end);
+			result.resize(in.tellg());
+			in.seekg(0, std::ios::beg);
+			in.read(&result[0], result.size());
+			in.close();
+		}
+		else
+		{
+			SHUNYA_CORE_ERROR("(Loacation - OpenGLShader) Couldn't open file {0}", filepath);
 		}
 
-		// Create an empty fragment shader handle
-		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		return result;
 
-		// Send the fragment shader source code to GL
-		// Note that std::string's .c_str is NULL character terminated.
-		source = fragmentSrc.c_str();
-		glShaderSource(fragmentShader, 1, &source, 0);
+	}
 
-		// Compile the fragment shader
-		glCompileShader(fragmentShader);
+	std::unordered_map<GLenum, std::string> OpenGLShader::PreProcess(const std::string& source)
+	{
+		std::unordered_map<GLenum, std::string >shaderSources;
 
-		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
-		if (isCompiled == GL_FALSE)
+		const char* typeToken = "#type";
+		size_t typeTokenLength = strlen(typeToken);
+		size_t pos = source.find(typeToken, 0);
+		while (pos != std::string::npos)
 		{
-			GLint maxLength = 0;
-			glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
+			size_t eol = source.find_first_of("\r\n", pos);
+			SHUNYA_CORE_ASSERT(eol != std::string::npos, "Syntax error");
+			size_t begin = pos + typeTokenLength + 1;
+			std::string type = source.substr(begin, eol - begin);
+			// Inside the while loop...
+			SHUNYA_CORE_ASSERT(type == "vertex" || type == "fragment" || type == "fragement" || type == "pixel", "Invalid Shader type specified");
 
-			// The maxLength includes the NULL character
-			std::vector<GLchar> infoLog(maxLength);
-			glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+			size_t nextLinePos = source.find_first_of("\r\n", eol);
+			pos = source.find(typeToken, nextLinePos);
+			shaderSources[ShaderTypeFromString(type)] = source.substr(nextLinePos, pos- (nextLinePos == std::string::npos ? source.size()-1 : nextLinePos));
 
-			// We don't need the shader anymore.
-			glDeleteShader(fragmentShader);
-			// Either of them. Don't leak shaders.
-			glDeleteShader(vertexShader);
-
-			// Use the infoLog as you see fit.
-			SHUNYA_CORE_ERROR("{0}", infoLog.data());
-			SHUNYA_CORE_ASSERT(false, "Vertex failed");
-
-			return;
-
-			// In this simple program, we'll just 
 		}
+		return shaderSources;
+
+
+	}
+	void OpenGLShader::Compile(const std::unordered_map<GLenum, std::string >& shaderSources)
+	{
+		GLuint program= glCreateProgram();
+
+		std::vector<GLenum> glshaderIDs;
+		glshaderIDs.reserve(shaderSources.size());
+		for (auto& kv : shaderSources)
+		{
+			GLenum type = kv.first;
+			const std::string& source = kv.second;
+
+			GLuint shader = glCreateShader(type);
+		
+			const GLchar* sourceCStr = source.c_str();
+			glShaderSource(shader, 1, &sourceCStr, 0);
+
+			
+			glCompileShader(shader);
+
+			GLint isCompiled = 0;
+			glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+			// Inside Compile function...
+			if (isCompiled == GL_FALSE)
+			{
+				GLint maxLength = 0;
+				glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+
+				// FIX: Handle cases where no log is returned
+				if (maxLength > 0)
+				{
+					std::vector<GLchar> infoLog(maxLength);
+					glGetShaderInfoLog(shader, maxLength, &maxLength, &infoLog[0]);
+					SHUNYA_CORE_ERROR("{0}", infoLog.data());
+				}
+
+				glDeleteShader(shader);
+				break;
+			}
+
+			glAttachShader(program, shader);
+			glshaderIDs.push_back(shader);
+
+		}
+		//--------------------------------------
 
 		// Vertex and fragment shaders are successfully compiled.
 		// Now time to link them together into a program.
 		// Get a program object.
-		m_RendererID = glCreateProgram();
-		GLuint program = m_RendererID;
 
-		// Attach our shaders to our program
-		glAttachShader(program, vertexShader);
-		glAttachShader(program, fragmentShader);
 
 		// Link our program
 		glLinkProgram(program);
@@ -105,27 +161,24 @@ namespace Shunya
 			// We don't need the program anymore.
 			glDeleteProgram(program);
 			// Don't leak shaders either.
-			glDeleteShader(vertexShader);
-			glDeleteShader(fragmentShader);
+
+			for (auto id : glshaderIDs)
+				glDeleteShader(id);
 
 			// Use the infoLog as you see fit.
 			SHUNYA_CORE_ERROR("{0}", infoLog.data());
 			SHUNYA_CORE_ASSERT(false, "Vertex failed");
 			// In this simple program, we'll just leave
 			return;
+
 		}
+		for (auto id : glshaderIDs)
+			glDetachShader(program, id);
 
-		// Always detach shaders after a successful link.
-		glDetachShader(program, vertexShader);
-		glDetachShader(program, fragmentShader);
-
+		m_RendererID = program;
 	}
-	OpenGLShader::~OpenGLShader()
-	{
 
-		glDeleteProgram(m_RendererID);
 
-	}
 	void OpenGLShader::Bind() const
 	{
 		glUseProgram(m_RendererID);
