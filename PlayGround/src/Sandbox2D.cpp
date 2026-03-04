673ac0f697410ec24d0d56ff01fa8eec9741207e
Abhimanyu Sharma <abhimanyu254a@gmail.com>
2026-03-04 23:05:13 +0530

    Worked on the 2D Renderer

--- PlayGround/src/Sandbox2D.cpp
+++ PlayGround/src/Sandbox2D.cpp
@@ -43,9 +43,9 @@ void Sandbox2D::OnUpdate(Shunya::Timestamp ts)
         Shunya::Renderer2D::BeginScene(m_CameraController.GetCamera());
     }
     
-    Shunya::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+    Shunya::Renderer2D::DrawRotateQuad({ -1.0f, 0.0f },  glm::radians(-45.0f), { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
     Shunya::Renderer2D::DrawQuad({ 0.5f,-0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-    Shunya::Renderer2D::DrawQuad({ 0.0f,0.0f ,-0.1f}, { 10.f, 10.0f }, m_Texture);
+    Shunya::Renderer2D::DrawQuad({ 0.0f,0.0f ,-0.1f} ,{ 10.f, 10.0f }, m_Texture, 10.0f/*,glm::vec4(1.0f,0.9f,0.9f,1.0f)*/);
     Shunya::Renderer2D::EndScene();   
 
 
