8990902a81f941c700d2e809fb93e0b405e93cb9
Abhimanyu Sharma <abhimanyu254a@gmail.com>
2026-03-07 21:31:38 +0530

    Started the Batch Renderering

--- Shunya-Core/src/Core/Rendered/Buffer.cpp
+++ Shunya-Core/src/Core/Rendered/Buffer.cpp
@@ -6,13 +6,14 @@
 
 namespace Shunya
 {
-	VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size) {
 
+	Ref<VertexBuffer> VertexBuffer::Create(uint32_t size)
+	{
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None: 			SHUNYA_CORE_ASSERT(false, "RendererAPI is not found or not Implemented Right now"); return nullptr;
 
-		case RendererAPI::API::OpenGL:  			return new OpenglVertexBuffer(vertices, size);
+		case RendererAPI::API::OpenGL:  			return CreateRef<OpenglVertexBuffer>(size);
 		case RendererAPI::API::DirectX12:		SHUNYA_CORE_ASSERT(false, "DirectX12 is not Implemented Right now"); return nullptr;
 		case RendererAPI::API::Vulkan: 			SHUNYA_CORE_ASSERT(false, "Vulkan is not Implemented Right now"); return nullptr;
 		case RendererAPI::API::Metal: 			SHUNYA_CORE_ASSERT(false, "Metal is not Implemented Right now"); return nullptr;
@@ -23,12 +24,29 @@ namespace Shunya
 		return nullptr;
 
 	}
-	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t size)
+	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size) {
+
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None: 			SHUNYA_CORE_ASSERT(false, "RendererAPI is not found or not Implemented Right now"); return nullptr;
+
+		case RendererAPI::API::OpenGL:  			return CreateRef<OpenglVertexBuffer>(vertices, size);
+		case RendererAPI::API::DirectX12:		SHUNYA_CORE_ASSERT(false, "DirectX12 is not Implemented Right now"); return nullptr;
+		case RendererAPI::API::Vulkan: 			SHUNYA_CORE_ASSERT(false, "Vulkan is not Implemented Right now"); return nullptr;
+		case RendererAPI::API::Metal: 			SHUNYA_CORE_ASSERT(false, "Metal is not Implemented Right now"); return nullptr;
+
+		}
+
+		SHUNYA_CORE_ASSERT(false, "Unknown RendererAPI");
+		return nullptr;
+
+	}
+	Ref<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t size)
 	{
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None: 			SHUNYA_CORE_ASSERT(false, "RendererAPI is not found or not Implemented Right now"); return nullptr;
-		case RendererAPI::API::OpenGL:  			return new OpenglIndexBuffer(indices, size);
+		case RendererAPI::API::OpenGL:  			return CreateRef<OpenglIndexBuffer>(indices, size);
 		case RendererAPI::API::DirectX12:		SHUNYA_CORE_ASSERT(false, "DirectX12 is not Implemented Right now"); return nullptr;
 		case RendererAPI::API::Vulkan: 			SHUNYA_CORE_ASSERT(false, "Vulkan is not Implemented Right now"); return nullptr;
 		case RendererAPI::API::Metal: 			SHUNYA_CORE_ASSERT(false, "Metal is not Implemented Right now"); return nullptr;
