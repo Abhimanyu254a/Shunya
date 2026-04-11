375dff1a0b3550169deec4e6fffdae45f64109d7
Abhimanyu Sharma <abhimanyu254a@gmail.com>
2026-04-11 23:55:05 +0530

    Worked on the Camera Component UI

--- Shunya-Editor/src/EditorLayer.cpp
+++ Shunya-Editor/src/EditorLayer.cpp
@@ -29,9 +29,12 @@ namespace Shunya {
         
         m_ActiveScene = std::make_shared<Scene>();
 
-        m_SquareEntity = m_ActiveScene->CreateEntity("Square");
+        m_SquareEntity = m_ActiveScene->CreateEntity("Blue Square");
         m_SquareEntity.AddComponent<SpriteRendererComponent>(
             glm::vec4{ 0.2f, 0.3f, 0.8f, 1.0f });
+
+        auto redSquare = m_ActiveScene->CreateEntity("Red Square");
+        redSquare.AddComponent<SpriteRendererComponent>(glm::vec4{ 1.0f, 0.0f, 0.0f, 1.0f });
 
         m_CameraEntity = m_ActiveScene->CreateEntity("Camera Entity");
         m_CameraEntity.AddComponent<CameraComponent>();
